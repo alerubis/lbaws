33e25dffd7c41eca4b1decdab2fe4977751027f0
AleSalvi <alessandr023salvi@gmail.com>
2025-03-15 00:00:13 +0100

    view

---
+++ src/routes/db/auto/player_year_league_summary_seconds_play-routes.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import { authenticateToken } from '../../../shared/auth';
+import { wrapAsync } from '../../../shared/functions';
+import { JSend } from '../../../shared/jsend';
+
+const prisma = new PrismaClient();
+
+const router = express.Router();
+router.use(authenticateToken());
+
+router.post('/read', wrapAsync(async (req: any, res: any) => {
+    const response = await prisma.$transaction(async (tx) => {
+        const rows = await tx.player_year_league_summary_seconds_play.findMany({
+            skip: req.body?.skip,
+            take: req.body?.take || 1,
+            where: req.body?.where,
+            orderBy: req.body?.orderBy,
+        });
+        const count = await tx.player_year_league_summary_seconds_play.count({ where: req.body?.where });
+        return {
+            rows: rows,
+            count: count,
+        };
+    });
+    res.status(200).json(JSend.success(response));
+}));
+
+export default router;
