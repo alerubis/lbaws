a25069f296fffe50b93fb7a754c9684d07313c57
AleSalvi <alessandr023salvi@gmail.com>
2025-04-04 14:47:48 +0200

    as

--- prisma/views/lba/v_team_game_absolute_minute_boxscore.sql
+++ prisma/views/lba/v_team_game_absolute_minute_boxscore.sql
@@ -5,40 +5,46 @@ SELECT
   `base`.`league_year_id` AS `league_year_id`,
   `base`.`league_id` AS `league_id`,
   `base`.`absolute_minute` AS `absolute_minute`,
-  sum(`base`.`fouls_committed`) AS `fouls_committed`,
-  sum(`base`.`fouls_received`) AS `fouls_received`,
-  sum(`base`.`points`) AS `points`,
-  sum(`base`.`made_2pt`) AS `made_2pt`,
-  sum(`base`.`missed_2pt`) AS `missed_2pt`,
-  round(
-    (
-      (sum(`base`.`made_2pt`) * 100.0) / nullif(sum((`base`.`made_2pt` + `base`.`missed_2pt`)), 0)
-    ),
-    1
+  cast(sum(`base`.`fouls_committed`) AS decimal(10, 0)) AS `fouls_committed`,
+  cast(sum(`base`.`fouls_received`) AS decimal(10, 0)) AS `fouls_received`,
+  cast(sum(`base`.`points`) AS decimal(10, 0)) AS `points`,
+  cast(sum(`base`.`made_2pt`) AS decimal(10, 0)) AS `made_2pt`,
+  cast(sum(`base`.`missed_2pt`) AS decimal(10, 0)) AS `missed_2pt`,
+  cast(
+    round(
+      (
+        (sum(`base`.`made_2pt`) * 100.0) / nullif(sum((`base`.`made_2pt` + `base`.`missed_2pt`)), 0)
+      ),
+      1
+    ) AS decimal(10, 0)
   ) AS `pct_2pt`,
-  sum(`base`.`made_3pt`) AS `made_3pt`,
-  sum(`base`.`missed_3pt`) AS `missed_3pt`,
-  round(
-    (
-      (sum(`base`.`made_3pt`) * 100.0) / nullif(sum((`base`.`made_3pt` + `base`.`missed_3pt`)), 0)
-    ),
-    1
+  cast(sum(`base`.`made_3pt`) AS decimal(10, 0)) AS `made_3pt`,
+  cast(sum(`base`.`missed_3pt`) AS decimal(10, 0)) AS `missed_3pt`,
+  cast(
+    round(
+      (
+        (sum(`base`.`made_3pt`) * 100.0) / nullif(sum((`base`.`made_3pt` + `base`.`missed_3pt`)), 0)
+      ),
+      1
+    ) AS decimal(10, 0)
   ) AS `pct_3pt`,
-  sum(`base`.`made_ft`) AS `made_ft`,
-  sum(`base`.`missed_ft`) AS `missed_ft`,
-  round(
-    (
-      (sum(`base`.`made_ft`) * 100.0) / nullif(sum((`base`.`made_ft` + `base`.`missed_ft`)), 0)
-    ),
-    1
+  cast(sum(`base`.`made_ft`) AS decimal(10, 0)) AS `made_ft`,
+  cast(sum(`base`.`missed_ft`) AS decimal(10, 0)) AS `missed_ft`,
+  cast(
+    round(
+      (
+        (sum(`base`.`made_ft`) * 100.0) / nullif(sum((`base`.`made_ft` + `base`.`missed_ft`)), 0)
+      ),
+      1
+    ) AS decimal(10, 0)
   ) AS `pct_ft`,
-  sum(`base`.`off_reb`) AS `off_reb`,
-  sum(`base`.`def_reb`) AS `def_reb`,
-  sum(`base`.`blocks_made`) AS `blocks_made`,
-  sum(`base`.`blocks_suffered`) AS `blocks_suffered`,
-  sum(`base`.`turnovers`) AS `turnovers`,
-  sum(`base`.`steals`) AS `steals`,
-  sum(`base`.`assists`) AS `assists`
+  cast(sum(`base`.`off_reb`) AS decimal(10, 0)) AS `off_reb`,
+  cast(sum(`base`.`def_reb`) AS decimal(10, 0)) AS `def_reb`,
+  cast(sum(`base`.`blocks_made`) AS decimal(10, 0)) AS `blocks_made`,
+  cast(sum(`base`.`blocks_suffered`) AS decimal(10, 0)) AS `blocks_suffered`,
+  cast(sum(`base`.`turnovers`) AS decimal(10, 0)) AS `turnovers`,
+  cast(sum(`base`.`steals`) AS decimal(10, 0)) AS `steals`,
+  cast(sum(`base`.`assists`) AS decimal(10, 0)) AS `assists`
 FROM
   (
     SELECT
--- prisma/views/lba/v_team_game_total_boxscore.sql
+++ prisma/views/lba/v_team_game_total_boxscore.sql
@@ -4,75 +4,113 @@ SELECT
   `lba`.`v_team_game_minute_boxscore`.`type_game_id` AS `type_game_id`,
   `lba`.`v_team_game_minute_boxscore`.`league_year_id` AS `league_year_id`,
   `lba`.`v_team_game_minute_boxscore`.`league_id` AS `league_id`,
-  sum(
-    `lba`.`v_team_game_minute_boxscore`.`fouls_committed`
+  cast(
+    sum(
+      `lba`.`v_team_game_minute_boxscore`.`fouls_committed`
+    ) AS decimal(10, 0)
   ) AS `fouls_committed`,
-  sum(
-    `lba`.`v_team_game_minute_boxscore`.`fouls_received`
+  cast(
+    sum(
+      `lba`.`v_team_game_minute_boxscore`.`fouls_received`
+    ) AS decimal(10, 0)
   ) AS `fouls_received`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`points`) AS `points`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`made_2pt`) AS `made_2pt`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`missed_2pt`) AS `missed_2pt`,
-  round(
-    (
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`points`) AS decimal(10, 0)
+  ) AS `points`,
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`made_2pt`) AS decimal(10, 0)
+  ) AS `made_2pt`,
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`missed_2pt`) AS decimal(10, 0)
+  ) AS `missed_2pt`,
+  cast(
+    round(
       (
-        sum(`lba`.`v_team_game_minute_boxscore`.`made_2pt`) * 100.0
-      ) / nullif(
-        sum(
-          (
-            `lba`.`v_team_game_minute_boxscore`.`made_2pt` + `lba`.`v_team_game_minute_boxscore`.`missed_2pt`
-          )
-        ),
-        0
-      )
-    ),
-    1
+        (
+          sum(`lba`.`v_team_game_minute_boxscore`.`made_2pt`) * 100.0
+        ) / nullif(
+          sum(
+            (
+              `lba`.`v_team_game_minute_boxscore`.`made_2pt` + `lba`.`v_team_game_minute_boxscore`.`missed_2pt`
+            )
+          ),
+          0
+        )
+      ),
+      1
+    ) AS decimal(10, 0)
   ) AS `pct_2pt`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`made_3pt`) AS `made_3pt`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`missed_3pt`) AS `missed_3pt`,
-  round(
-    (
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`made_3pt`) AS decimal(10, 0)
+  ) AS `made_3pt`,
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`missed_3pt`) AS decimal(10, 0)
+  ) AS `missed_3pt`,
+  cast(
+    round(
       (
-        sum(`lba`.`v_team_game_minute_boxscore`.`made_3pt`) * 100.0
-      ) / nullif(
-        sum(
-          (
-            `lba`.`v_team_game_minute_boxscore`.`made_3pt` + `lba`.`v_team_game_minute_boxscore`.`missed_3pt`
-          )
-        ),
-        0
-      )
-    ),
-    1
+        (
+          sum(`lba`.`v_team_game_minute_boxscore`.`made_3pt`) * 100.0
+        ) / nullif(
+          sum(
+            (
+              `lba`.`v_team_game_minute_boxscore`.`made_3pt` + `lba`.`v_team_game_minute_boxscore`.`missed_3pt`
+            )
+          ),
+          0
+        )
+      ),
+      1
+    ) AS decimal(10, 0)
   ) AS `pct_3pt`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`made_ft`) AS `made_ft`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`missed_ft`) AS `missed_ft`,
-  round(
-    (
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`made_ft`) AS decimal(10, 0)
+  ) AS `made_ft`,
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`missed_ft`) AS decimal(10, 0)
+  ) AS `missed_ft`,
+  cast(
+    round(
       (
-        sum(`lba`.`v_team_game_minute_boxscore`.`made_ft`) * 100.0
-      ) / nullif(
-        sum(
-          (
-            `lba`.`v_team_game_minute_boxscore`.`made_ft` + `lba`.`v_team_game_minute_boxscore`.`missed_ft`
-          )
-        ),
-        0
-      )
-    ),
-    1
+        (
+          sum(`lba`.`v_team_game_minute_boxscore`.`made_ft`) * 100.0
+        ) / nullif(
+          sum(
+            (
+              `lba`.`v_team_game_minute_boxscore`.`made_ft` + `lba`.`v_team_game_minute_boxscore`.`missed_ft`
+            )
+          ),
+          0
+        )
+      ),
+      1
+    ) AS decimal(10, 0)
   ) AS `pct_ft`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`off_reb`) AS `off_reb`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`def_reb`) AS `def_reb`,
-  sum(
-    `lba`.`v_team_game_minute_boxscore`.`blocks_made`
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`off_reb`) AS decimal(10, 0)
+  ) AS `off_reb`,
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`def_reb`) AS decimal(10, 0)
+  ) AS `def_reb`,
+  cast(
+    sum(
+      `lba`.`v_team_game_minute_boxscore`.`blocks_made`
+    ) AS decimal(10, 0)
   ) AS `blocks_made`,
-  sum(
-    `lba`.`v_team_game_minute_boxscore`.`blocks_suffered`
+  cast(
+    sum(
+      `lba`.`v_team_game_minute_boxscore`.`blocks_suffered`
+    ) AS decimal(10, 0)
   ) AS `blocks_suffered`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`turnovers`) AS `turnovers`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`steals`) AS `steals`,
-  sum(`lba`.`v_team_game_minute_boxscore`.`assists`) AS `assists`
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`turnovers`) AS decimal(10, 0)
+  ) AS `turnovers`,
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`steals`) AS decimal(10, 0)
+  ) AS `steals`,
+  cast(
+    sum(`lba`.`v_team_game_minute_boxscore`.`assists`) AS decimal(10, 0)
+  ) AS `assists`
 FROM
   `lba`.`v_team_game_minute_boxscore`
 GROUP BY
